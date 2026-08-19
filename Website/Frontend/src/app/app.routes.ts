d709c34785506f796dd0d284fcda0e27ad1f0f47
fabianjoos1 <f.joos@students.htl-leonding.ac.at>
2026-08-19 12:15:12 +0200

    Frontend - Startpage + Routing implemented

--- Website/Frontend/src/app/app.routes.ts
+++ Website/Frontend/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { Start } from './start/start';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Start },
+  { path: '**', component: Start },
+];
--- Website/Frontend/src/app/app.ts
+++ Website/Frontend/src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Start } from './start/start';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Start],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('Frontend');
