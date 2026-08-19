75f0e0a4fee74a0cf5a1648bf52b0a6185a1385d
NiiloRieser <n.rieser@students.htl-leonding.ac.at>
2026-08-19 12:46:56 +0200

    Routing erweitert

--- Website/Frontend/src/app/app.routes.ts
+++ Website/Frontend/src/app/app.routes.ts
@@ -1,7 +1,15 @@
 import { Routes } from '@angular/router';
 import { Start } from './start/start';
+import {Home} from './home/home';
+import {Games} from './games/games';
+import {CreateGame} from './create-game/create-game';
+import {MyGames} from './my-games/my-games';
 
 export const routes: Routes = [
   { path: '', component: Start },
+  { path: 'home', component: Home },
+  { path: 'games', component: Games },
+  { path: 'myGames', component: MyGames },
+  { path: 'createGame', component: CreateGame },
   { path: '**', component: Start },
 ];
