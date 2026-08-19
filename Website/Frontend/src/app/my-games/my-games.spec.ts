caa3df14e76d96cfdd9ffc731e6f4004053a04a0
NiiloRieser <n.rieser@students.htl-leonding.ac.at>
2026-08-19 13:17:19 +0200

    My-games verzögert hinzugefügt

---
+++ Website/Frontend/src/app/my-games/my-games.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MyGames } from './my-games';
+
+describe('MyGames', () => {
+  let component: MyGames;
+  let fixture: ComponentFixture<MyGames>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [MyGames],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(MyGames);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ Website/Frontend/src/app/my-games/my-games.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-games',
+  imports: [],
+  templateUrl: './my-games.html',
+  styleUrl: './my-games.css',
+})
+export class MyGames {}
