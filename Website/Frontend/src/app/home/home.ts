d587f135c8cd4f3e47335a738138341b084debce
DavidBerghahn <berghahn.david@icloud.com>
2026-08-20 20:02:02 +0200

    Show token claims from platform home

--- Website/Frontend/src/app/home/home.ts
+++ Website/Frontend/src/app/home/home.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import {RouterLink} from '@angular/router';
 import { Footer } from '../footer/footer';
+import { AuthService, UserProfile } from '../auth.service';
 
 @Component({
   selector: 'app-home',
@@ -8,4 +9,24 @@ import { Footer } from '../footer/footer';
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home implements OnInit {
+  private readonly authService = inject(AuthService);
+
+  profile: UserProfile | null = null;
+  debugVisible = false;
+  debugText = '';
+  authError = '';
+
+  async ngOnInit(): Promise<void> {
+    try {
+      this.profile = await this.authService.loadProfile();
+      this.debugText = JSON.stringify(this.authService.debugInfo(this.profile), null, 2);
+    } catch (error) {
+      this.authError = error instanceof Error ? error.message : String(error);
+    }
+  }
+
+  toggleAuthDetails(): void {
+    this.debugVisible = !this.debugVisible;
+  }
+}
