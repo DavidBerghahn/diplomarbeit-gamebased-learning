1463d34a487d2f6e905107f6f17e456ef885a3ba
NiiloRieser <n.rieser@students.htl-leonding.ac.at>
2026-08-21 15:31:12 +0200

    Pfade aktualisiert

--- Website/Frontend/src/app/app.routes.ts
+++ Website/Frontend/src/app/app.routes.ts
@@ -5,10 +5,14 @@ import {Games} from './games/games';
 import {CreateGame} from './create-game/create-game';
 import {MyGames} from './my-games/my-games';
 import { Login } from './login/login';
+import {Quizbattle} from './quizbattle/quizbattle';
+import {Lobby} from './lobby/lobby';
 
 export const routes: Routes = [
   { path: '', component: Start },
   { path: 'login', component: Login},
+  { path: 'lobby', component: Lobby},
+  { path: 'quizbattle', component: Quizbattle},
   { path: 'home', component: Home },
   { path: 'games', component: Games },
   { path: 'myGames', component: MyGames },
